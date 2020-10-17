db07bb1f5a1698e1a13a1b9fa733f59f222b8d3c
Cameron Spence <cspence@usc.edu>
2020-10-16 22:09:25 -0700

    CameraFollow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = .125f;
+
+    public Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(0, 3, -10);
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPos = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+}
